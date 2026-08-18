068697b123544410f8a15c92c5082e20433808ca
mehta-g1 <vikash.kumarmehta.bca.2023@mitmeerut.ac.in>
2026-08-18 19:23:09 +0530

    creating a web app for model

---
+++ 29_Steps By Step Project Implementation With LifeCycle OF ML Project/web app/application.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify, render_template
+import pickle
+import numpy
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+
+application = Flask(__name__)
+app = application
+
+# import ridge regressor and standard scaler pickle
+linreg = pickle.load(open('models/linreg.pkl', 'rb'))
+standard_scaler = pickle.load(open('models/scaler.pkl', 'rb'))
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+@app.route('predictdata', methods = ['GET', 'POST'])
+def predict_datapoint():
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template('home.html')
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
