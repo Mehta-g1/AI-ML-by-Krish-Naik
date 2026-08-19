31cfd4a5bfe9e431d24c4c1978d670e8e0ee6abb
mehta-g1 <vikash.kumarmehta.bca.2023@mitmeerut.ac.in>
2026-08-19 06:59:40 +0530

    web app completed

--- 29_Steps By Step Project Implementation With LifeCycle OF ML Project/web app/application.py
+++ 29_Steps By Step Project Implementation With LifeCycle OF ML Project/web app/application.py
@@ -18,14 +18,48 @@ def index():
     return render_template('index.html')
 
 
-@app.route('predictdata', methods = ['GET', 'POST'])
+@app.route('/predictdata', methods = ['GET', 'POST'])
 def predict_datapoint():
     if request.method == 'POST':
-        pass
+        try: 
+            Temprature = float(request.form.get('Temperature'))
+            RH  = float(request.form.get('RH'))
+            Ws = float(request.form.get('Ws'))
+            Rain = float(request.form.get('Rain'))
+            FFMC = float(request.form.get('FFMC'))
+            DMC = float(request.form.get('DMC'))
+            ISI = float(request.form.get('ISI'))
+            Classes = float(request.form.get('Classes'))
+            Region = float(request.form.get('Region'))
+        
+
+            df = pd.DataFrame(
+                [[Temprature, RH, Ws, Rain, FFMC, DMC, ISI, Classes, Region]], 
+                columns=['Temperature', 'RH', 'Ws', 'Rain', 'FFMC', 'DMC', 'ISI', 'Classes', 'Region']
+            )
+            print('\n','='*60,'\n')
+            print('Without Scaled:\n', df)
+            new_data_scaled = standard_scaler.transform(df)
+            dict_data = df.to_dict(orient='records')[0]
+            print('Data After Scaled:\n', new_data_scaled)
+
+            print("Dict Data\n",dict_data)
+            print('\n','='*60,'\n')
+
+            result = linreg.predict(new_data_scaled)
+            
+        except Exception as e:
+            result = [f'Error: {e}']
+            dict_data = dict()
+
+
+        return render_template(
+            'home.html',
+            result=result[0],
+            data = dict_data
+        )
     else:
-        return render_template('home.html')
-
-
+        return render_template('home.html', data=None)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
